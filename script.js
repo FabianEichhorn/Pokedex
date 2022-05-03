f936abd228ac2d312969aa83a69bdde72f0f8cc5
FabianEichhorn <fabihorn.go@gmail.com>
2022-05-03 17:44:40 +0200

    function running with one type

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 let currentPokemon;
 
 async function loadPokemon() {
-    for (let i = 1; i < 2; i++) {
+    for (let i = 1; i < 20; i++) {
 
         let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
         let response = await fetch(url);
         currentPokemon = await response.json();
         console.log('loaded pokemon', currentPokemon);
-        let testurl = `https://pokeapi.co/api/v2/pokemon/1`;
+        let testurl = `https://pokeapi.co/api/v2/pokemon/4`;
         let testresponse = await fetch(testurl);
         bulbasaur = await testresponse.json();
-        console.log('test ', bulbasaur)
-        document.getElementById('pokemon').innerHTML = '';
-        for (let i = 1; i < 20; i++) {
+        console.log('test ', bulbasaur);
 
-            document.getElementById('pokemon').innerHTML += `
+        document.getElementById('pokemon').innerHTML += `
             <div class="pokemon-container" id="pokemon-container">
         <div class="poke-id">
                     <span class="pokemon-id">${currentPokemon['id']}</span>
@@ -24,7 +22,6 @@ async function loadPokemon() {
              
                         <span class="pokemon-name">${currentPokemon['name']}</span>
                         <span class="pokemon-type1">${currentPokemon['types'][0]['type']['name']}</span>
-                        <span class="pokemon-type2">${currentPokemon['types'][1]['type']['name']}</span>
                     </div>
                     <div class="pokemon-body-image">
                         <img class="pokemon-image" src=${currentPokemon['sprites']['other']['home']['front_default']} alt="">
@@ -33,8 +30,8 @@ async function loadPokemon() {
                 </div>
                 </div>
         `;
-        }
     }
+
 }
 
 
